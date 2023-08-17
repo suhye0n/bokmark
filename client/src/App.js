617118d9a2fdc81d57f3eb01c8e800a46c19511a
suhye0n <contact.suhyeon@gmail.com>
2023-08-17 23:40:30 +0900

    라우팅 기능 추가
    라우팅 기능 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
